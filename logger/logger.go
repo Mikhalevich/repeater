e6cbe26e4c8b9bab2935cb7038c1f136a37a3975
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2025-06-25 19:07:11 +0300

    example error doer func

--- logger/logger.go
+++ logger/logger.go
@@ -2,7 +2,6 @@ package logger
 
 // Logger interface for external implementation to enable logging for outbox.
 type Logger interface {
-	Debug(msg string, args ...any)
 	Error(msg string, args ...any)
 
 	WithError(err error) Logger
